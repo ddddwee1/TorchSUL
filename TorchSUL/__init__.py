f388ca157f0a0a630b20200fdf9fb67a397ccc9e
ddddwee1 <cy960823@outlook.com>
2020-11-14 17:27:44 +0800

    Update plotter

--- TorchSUL/__init__.py
+++ TorchSUL/__init__.py
@@ -3,11 +3,13 @@ from . import Layers
 from . import Model
 from . import sul_tool
 from . import sulio 
+from . import sulplotter
 
 __all__ = [
 	'DataReader',
 	'Layers',
 	'Model',
-	'sul_tool'
-	'sulio'
+	'sul_tool',
+	'sulio',
+	'sulplotter'
 ]
