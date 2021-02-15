77a645644821f2da16738d1447148212eb9379e1
Cheng Yu <cy960823@outlook.com>
2021-02-15 19:18:21 +0800

    Update sul_tool.py

--- TorchSUL/sul_tool.py
+++ TorchSUL/sul_tool.py
@@ -7,20 +7,23 @@ def extract_frames(fname,prefix,skip=1):
 	print('Extracting %s'%fname)
 	t1 = time.time()
 	cap = cv2.VideoCapture(fname)
+	framenum = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 	cnt = 0
 	images = []
-	while cap.isOpened():
-		ret, frame = cap.read()
-		if not ret:
-			break
-		if cnt%skip==0:
-			if prefix is None:
-				images.append(frame)
-			else:
-				imgname = prefix+'_%08d.jpg'%cnt
-				if not os.path.exists(imgname):
-					cv2.imwrite(imgname, frame)
-		cnt += 1
+	with progressbar.ProgressBar(max_value=framenum) as bar:
+		while cap.isOpened():
+			ret, frame = cap.read()
+			if not ret:
+				break
+			if cnt%skip==0:
+				if prefix is None:
+					images.append(frame)
+				else:
+					imgname = prefix+'_%08d.jpg'%cnt
+					if not os.path.exists(imgname):
+						cv2.imwrite(imgname, frame)
+			bar.update(cnt)
+			cnt += 1
 	t2 = time.time()
 	print('Extraction finished. Time elapsed:', t2-t1)
 	return images
