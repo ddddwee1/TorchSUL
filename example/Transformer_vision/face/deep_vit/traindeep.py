efdba006ac07405ac350cc9ea3b72b8deb98f0dc
Cheng Yu <cy960823@outlook.com>
2021-05-06 21:46:48 +0800

    Update transformer

---
+++ example/Transformer_vision/face/deep_vit/traindeep.py
@@ -0,0 +1,87 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3,4,5,6,7'
+import torch 
+import torch.nn as nn 
+import torch.nn.functional as F 
+from TorchSUL import Model as M 
+import numpy as np 
+from tqdm import tqdm 
+import losses 
+import datareader 
+from deepvit import DeepViT 
+
+if __name__=='__main__':
+	devices = (0,1,2,3,4,5,6,7)
+
+	BSIZE = 256 * 8
+	reader = datareader.get_datareader(BSIZE, processes=16)
+	# reader = datareader
+
+	BackboneRes100 = DeepViT(image_size=112, patch_size=16, dim=512, depth=16, heads=16, mlp_dim=2048, dropout=0.1, emb_dropout=0.1)
+	classifier = losses.DistributedClassifier(reader.max_label, devices)
+
+	# init 
+	dumb_x = torch.rand(2, 3, 112, 112)
+	dumb_y = torch.from_numpy(np.int64(np.zeros(2)))
+	dumb_f = BackboneRes100(dumb_x)
+	_ = classifier(dumb_f, dumb_y)
+	print(dumb_f.shape)
+
+	# restore 
+	if devices is not None: 
+		BackboneRes100 = nn.DataParallel(BackboneRes100, device_ids=devices).cuda()
+		classifier = classifier.cuda()
+	saver = M.Saver(BackboneRes100)
+	saver_classifier = M.Saver(classifier)
+	saver.restore('./model/')
+	saver_classifier.restore('./classifier/')
+
+	# define optim 
+	# optim = torch.optim.SGD([{'params':BackboneRes100.parameters()}, {'params':classifier.parameters()}], lr=0.001, momentum=0.9, weight_decay=0.0005)
+	optim = torch.optim.AdamW([{'params':BackboneRes100.parameters()}, {'params':classifier.parameters()}], lr=0.001, weight_decay=0.0005)
+
+	classifier.train()
+	BackboneRes100.train()
+
+	MAXEPOCH = 45
+
+	for ep in range(MAXEPOCH):
+		if ep<5:
+			m2 = 0.0
+		else:
+			m2 = 0.5
+		print('m2=',m2)
+		bar = tqdm(range(reader.iter_per_epoch))
+		for it in bar:
+
+			imgs, labels = reader.get_next()
+			# imgs, labels = reader.post_process(123)
+			imgs = torch.from_numpy(imgs)
+			labels = torch.from_numpy(labels)
+
+			# training loop 
+			optim.zero_grad()
+			feats = BackboneRes100(imgs)
+			loss, acc = classifier(feats, labels, m2=m2)
+			loss.backward()
+			optim.step()
+
+			# output string 
+			lr = optim.param_groups[0]['lr']
+			loss = loss.cpu().detach().numpy()
+			# acc = acc.cpu().detach().numpy()
+			outstr = 'Ep:%d Ls:%.3f Ac:%.3f Lr:%.1e'%(ep, loss, acc, lr)
+			bar.set_description(outstr)
+
+			# save model 
+			if it%8000==0 and it>0:
+				saver.save('./model/dvit_%d_%d.pth'%(ep, it))
+				saver_classifier.save('./classifier/classifier_%d_%d.pth'%(ep,it))
+
+		saver.save('./model/dvit_%d_%d.pth'%(ep, it))
+		saver_classifier.save('./classifier/classifier_%d_%d.pth'%(ep,it))
+
+		if ep%25==14:
+			newlr = lr * 0.1 
+			for param_group in optim.param_groups:
+				param_group['lr'] = newlr
---
+++ example/Transformer_vision/face/vit/datareader.py
@@ -0,0 +1,121 @@
+import numpy as np 
+import cv2 
+from TorchSUL import DataReader
+import random 
+from TorchSUL import sulio 
+
+ioout = sulio.DataFrame('../data_emore_sul/', debug=True)
+block_size = 32
+stride = 16
+n_grid = 6
+
+def adjust_img(img):
+	# img = img[:16*4]
+	if random.random()>0.5:
+		img = np.flip(img, axis=1)
+	img = np.float32(img)
+	img = img / 127.5 - 1.
+	img = np.transpose(img, (2,0,1))
+	# res = []
+	# for i in range(n_grid):
+	# 	for j in range(n_grid):
+	# 		start_h = i*stride
+	# 		start_w = j*stride
+	# 		res.append(img[:,start_h:start_h+block_size, start_w:start_w+block_size])
+	# res = np.float32(res)
+	res = img 
+	return res 
+
+def pre_process(inp):
+	idx, labels = list(zip(*inp))
+	frames = []
+	for i in idx:
+		# print(i)
+		img = ioout.read_data(i)
+		frames.append(img)
+		# print(type(img))
+	pack = list(zip(*[frames, labels]))
+	return pack
+
+def process(sample):
+	# add more process here
+	img, lab = sample
+	img = np.frombuffer(img, dtype=np.uint8)
+	img = cv2.imdecode(img, cv2.IMREAD_COLOR)
+	img = adjust_img(img)
+	label = int(lab)
+	return img, label
+
+def post_process(inp):
+	res = list(zip(*inp))
+	imgs = np.float32(res[0])
+	lb = np.int64(res[1])
+	res = [imgs, lb]
+	return res 
+
+def get_data(img_thresh=0):
+	result = []	
+	_,header0 = ioout.read(0)
+
+	lab = 0
+	for idd in range(header0[0], header0[1]):
+		_, header = ioout.read(idd)
+
+		imgrange = (header[0], header[1])
+		if imgrange[1]-imgrange[0]<img_thresh:
+			continue
+		else:
+			buff = list(range(imgrange[0], imgrange[1]))
+			for item in buff:
+				result.append([item, lab])
+		lab += 1
+		# if lab==20000:
+		# 	break 
+	return result, lab
+
+def get_datareader(bsize, processes):
+	reader = DataReader.DataReader(bsize, processes=processes, gpus=1, sample_policy='EPOCH')
+	data, max_label = get_data()
+	print('MAX LABEL:', max_label)
+	print('DATA NUM:', len(data))
+	reader.set_data(data)
+	reader.set_param({'max_label':max_label})
+	reader.set_pre_process_fn(pre_process)
+	reader.set_process_fn(process)
+	reader.set_post_process_fn(post_process)
+	reader.prefetch()
+	return reader
+
+if __name__=='__main__':
+	# s = imgrec.read_idx(1000)
+	# hdd, img = recordio.unpack(s)
+	# img = np.frombuffer(img, dtype=np.uint8)
+	# img = cv2.imdecode(img, cv2.IMREAD_COLOR)
+	# cv2.imwrite('abc.jpg', img)
+	# label = int(hdd.label)
+	# print(label)
+
+	# import time 
+	# reader = get_datareader(16, 4)
+	# t1 = time.time()
+	# for i in range(100):
+	# 	batch = reader.get_next()
+	# 	print(i)
+	# t2 = time.time()
+	# print('TIME',t2-t1)
+	# print(reader.iter_per_epoch, reader.max_label, len(reader.data))
+	# print(batch[0].shape, batch[0].min(), batch[0].max())
+	# print(batch[1].shape)
+	# print(batch[1])
+
+	from time import time 
+
+	reader = get_datareader(1024, 4)
+
+	for i in range(2):
+		t1 = time()
+		batch = reader.get_next()
+		t2 = time()
+		print(t2-t1)
+	print(batch[0].shape)
+	print(batch[1].shape)
