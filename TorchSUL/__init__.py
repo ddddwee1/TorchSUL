3d426505d4f83f00d03f0c8126e95530b91965d7
Cheng Yu <cy960823@outlook.com>
2024-01-11 13:32:34 +0800

    Update __init__.py

--- TorchSUL/__init__.py
+++ TorchSUL/__init__.py
@@ -3,3 +3,12 @@ from . import Base, Model, Quant, Tools
 
 sul_tool = Tools
 
+# modify loguru format. Add \n in front, in case other modules overrides stderr
+import sys 
+from loguru import logger 
+
+logger.remove()
+fmt = '\n<g>{time:YYYY-MM-DD HH:mm:ss}</g> | PID: <c>{process}</c> | <lvl>{level}</lvl> - <lvl>{message}</lvl> '
+logger.add(sys.stderr, format=fmt)
+
+
