5e3da4fa10d447e9743b2b4d0117d8111abd1602
Cheng Yu <cy960823@outlook.com>
2024-01-25 18:16:36 +0800

    Update Types.py

--- TorchSUL/Consts/Types.py
+++ TorchSUL/Consts/Types.py
@@ -11,7 +11,7 @@ TypeKSize3D = Union[int, List[int], Tuple[int,int,int]]
 ## Quant type declarations
 QuantModes = Literal['layer_wise', 'channel_wise']
 QBitTypes = Literal['uint8', 'int8', 'int16']
-QObserverTypes = Literal['minmax', 'percentile', 'omse']
+QObserverTypes = Literal['minmax', 'percentile', 'omse', 'placeholder']
 QuantizerTypes = Literal['uniform']
 
 ## Image-video related 
@@ -19,4 +19,4 @@ BasicImageTypes = Literal['jpg', 'png', 'jpeg']
 
 # These are pre-set flags for SULModel 
 PRESET_FLAGS = Literal['save_tensor', 'loose_load', 'from_torch', 'fc2conv', 'QActBit', 'QActObserver',\
-                        'conv_init_mode', 'fc_init_mode', 'bn_eps', 'ln_eps', 'dump_onnx']
+                        'conv_init_mode', 'fc_init_mode', 'bn_eps', 'ln_eps', 'dump_onnx', 'LayerwiseQuant', 'CalibScoreFunc']
